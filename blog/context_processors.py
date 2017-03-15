1529aa4bf3350f1ba55f22862cf26ffaae3f27c2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-03-15 22:55:11 +1100

    Display pod host name in pages.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,6 +1,10 @@
 import os
 
 BLOG_SITE_NAME = os.environ.get('BLOG_SITE_NAME', 'OpenShift Blog')
+KUBE_POD_NAME = os.environ.get('HOSTNAME', 'localhost')
 
 def site_information(request):
-    return { 'BLOG_SITE_NAME': BLOG_SITE_NAME }
+    return {
+        'BLOG_SITE_NAME': BLOG_SITE_NAME,
+        'KUBE_POD_NAME': KUBE_POD_NAME,
+    }
