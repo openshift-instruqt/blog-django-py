e192484dba70a9afbd99e1762c1b965605706132
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-03-16 11:24:21 +1100

    Allow banner colour to be set by environment variable.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,10 +1,12 @@
 import os
 
 BLOG_SITE_NAME = os.environ.get('BLOG_SITE_NAME', 'OpenShift Blog')
+BLOG_BANNER_COLOR = os.environ.get('BLOG_BANNER_COLOR', '')
 KUBE_POD_NAME = os.environ.get('HOSTNAME', 'localhost')
 
 def site_information(request):
     return {
         'BLOG_SITE_NAME': BLOG_SITE_NAME,
+        'BLOG_BANNER_COLOR': BLOG_BANNER_COLOR,
         'KUBE_POD_NAME': KUBE_POD_NAME,
     }
