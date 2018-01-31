4a05c41995179cd43179efc00c36c2ef733bbd5d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-01-31 19:19:25 +1100

    Explicitly set on delete action to be compatible with Django 2.0.

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     image = models.ImageField("Image", blank=True, null=True, upload_to="images/")
