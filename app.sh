deacbbf71d61be9e2b2f2914cb04ee4815ed3a84
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-07-05 21:36:04 +1000

    Add means to set capacity.

--- app.sh
+++ app.sh
@@ -9,6 +9,18 @@ ARGS="$ARGS --port $PORT"
 ARGS="$ARGS --document-root htdocs"
 ARGS="$ARGS --url-alias /media media"
 
+if [ x"$MOD_WSGI_PROCESSES" != x"" ]; then
+    ARGS="$ARGS --processes $MOD_WSGI_PROCESSES"
+fi
+
+if [ x"$MOD_WSGI_THREADS" != x"" ]; then
+    ARGS="$ARGS --threads $MOD_WSGI_THREADS"
+fi
+
+if [ x"$MOD_WSGI_MAX_CLIENTS" != x"" ]; then
+    ARGS="$ARGS --max-clients $MOD_WSGI_MAX_CLIENTS"
+fi
+
 if [ x"$MOD_WSGI_RELOAD_ON_CHANGES" != x"" ]; then
     ARGS="$ARGS --reload-on-changes"
 fi
